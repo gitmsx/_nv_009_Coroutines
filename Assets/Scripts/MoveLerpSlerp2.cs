81985079f69c5c02de2426d9ef85f731d66ef2c9
gitmsx <gitmsx@mail.ru>
2022-11-24 15:54:40 +0300

    Saved on 24.11.2022-15:54:40,19

--- Assets/Scripts/MoveLerpSlerp2.cs
+++ Assets/Scripts/MoveLerpSlerp2.cs
@@ -1,8 +1,9 @@
 using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class MoveLerpSlerp : MonoBehaviour
+public class MoveLerpSlerp2 : MonoBehaviour
 {
 
     [SerializeField] private GameObject Cube1;
@@ -13,56 +14,33 @@ public class MoveLerpSlerp : MonoBehaviour
     [SerializeField] private GameObject Cube6;
     [SerializeField] private GameObject Targett;
 
-    private Vector3 TargetTP2;
+    private Vector3 TargetTP;
     [SerializeField] private float speed = 0.9f;
-
-    [HideInInspector] private Text Text__info003;
-    [HideInInspector] private Text Text__info001;
-    [HideInInspector] private Text Text__info002;
-
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        TargetTP2 = Targett.transform.position;
-        Text__info001 = GameObject.Find("Text__info001").GetComponent<Text>();
-        Text__info002 = GameObject.Find("Text__info002").GetComponent<Text>();
-        Text__info003 = GameObject.Find("Text__info003").GetComponent<Text>();
-        Text__info001.text = "";
-        Text__info002.text = "";
-        Text__info003.text = "";
-        StartCoroutine("MoveTo", TargetTP2);
-
+        TargetTP = Targett.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        
+        MoveTo();
     }
 
 
 
-    IEnumerator MoveTo(Vector3 TargetTP)
+    void MoveTo()
     {
-        
-        while ( Vector3.Distance(  Cube1.transform.position,TargetTP) > 10.2f)
-        { 
         Cube1.transform.position = Vector3.Lerp(Cube1.transform.position, TargetTP, speed * Time.deltaTime);
         Cube2.transform.position = Vector3.Lerp(Cube2.transform.position, TargetTP, speed * Time.deltaTime);
         Cube3.transform.position = Vector3.Slerp(Cube3.transform.position, TargetTP, speed * Time.deltaTime);
         Cube4.transform.position = Vector3.Slerp(Cube4.transform.position, TargetTP, speed * Time.deltaTime);
         Cube5.transform.position = Vector3.MoveTowards(Cube5.transform.position, TargetTP,  (speed /Time.deltaTime) / 1000);
         Cube6.transform.position = Vector3.MoveTowards(Cube6.transform.position, TargetTP,  (speed /Time.deltaTime) / 1000);
-
-            Text__info001.text = Cube4.transform.position.ToString();
-            yield return null;
-        }
-        Text__info002.text = "WaitForSeconds(3f)";
-        yield return new WaitForSeconds(3f);
-        Text__info003.text = "WaitForSeconds(3f) Complete ";
 
     }
 }
