c1df8b71e977eb48846dafe32ec11dad558a07ca
gitmsx <gitmsx@mail.ru>
2022-11-24 15:09:12 +0300

    Saved on 24.11.2022-15:09:11,91

---
+++ Assets/Scripts/MoveLerpSlerp.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MoveLerpSlerp : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Cube1;
+    [SerializeField] private GameObject Cube2;
+    [SerializeField] private GameObject Cube3;
+    [SerializeField] private GameObject Cube4;
+    [SerializeField] private GameObject Cube5;
+    [SerializeField] private GameObject Cube6;
+    [SerializeField] private GameObject Targett;
+
+    private Vector3 TargetTP;
+    [SerializeField]  private  float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetTP = Targett.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTo();
+    }
+
+
+
+    void MoveTo()
+    {
+        Cube1.transform.position = Vector3.Lerp(transform.position, TargetTP, speed * Time.deltaTime);
+        Cube2.transform.position = Vector3.Lerp(transform.position, TargetTP, speed * Time.deltaTime);
+        Cube3.transform.position = Vector3.Slerp(transform.position, TargetTP, speed * Time.deltaTime);
+        Cube4.transform.position = Vector3.Slerp(transform.position, TargetTP, speed * Time.deltaTime);
+        Cube5.transform.position = Vector3.MoveTowards(transform.position, TargetTP, speed * Time.deltaTime);
+        Cube6.transform.position = Vector3.MoveTowards(transform.position, TargetTP, speed * Time.deltaTime);
+
+    }
+}
